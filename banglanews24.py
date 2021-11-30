3ce38a0798f9dee2a2255bf4f123bd2a90b32722
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2021-11-30 07:39:25 +0000

    Ooops.

--- banglanews24.py
+++ banglanews24.py
@@ -29,3 +29,24 @@ for index in range(1, 25000):
         elif j == 7:
             url = newspaper_base_url + "category/শিল্প-সাহিত্য/11?page=" + str(index)
         elif j == 8:
+            url = newspaper_base_url + "category/লাইফস্টাইল/12?page=" + str(index)
+        elif j == 9:
+            url = newspaper_base_url + "/category/পর্যটন/13?page=" + str(index)
+        elif j == 10:
+            url = newspaper_base_url + "/category/চট্টগ্রাম-প্রতিদিন/14?page=" + str(index)
+        elif j == 11:
+            url = newspaper_base_url + "category/আইন-আদালত/18?page=" + str(index)
+        elif j == 12:
+            url = newspaper_base_url + "category/ইচ্ছেঘুড়ি/8?page=" + str(index)
+        elif j == 13:
+            url = newspaper_base_url + "category/প্রবাস/17?page=" + str(index)
+        elif j == 14:
+            url = newspaper_base_url + "category/স্বাস্থ্য/19?page=" + str(index)
+        elif j == 15:
+            url = newspaper_base_url + "category/শিক্ষা/20?page=" + str(index)
+        elif j == 16:
+            url = newspaper_base_url + "category/ইসলাম/15?page=" + str(index)
+        elif j == 18:
+            url = newspaper_base_url + "category/মুক্তমত/16?page=" + str(index)
+        elif j == 19:
+            url = newspaper_base_url + "category/জলবায়ু-পরিবেশ/21?page=" + str(index)
