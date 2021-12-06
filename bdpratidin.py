fd1e62e11146bcaa73f47c9c98667ed9f49f27d9
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2021-12-06 20:58:36 +0000

    Fixed travis build

--- bdpratidin.py
+++ bdpratidin.py
@@ -19,3 +19,20 @@ for i in range(100):
 
     for j in range(28):
         if j == 0:
+            url = newspaper_base_url + "national/" + str(index)
+        elif j == 1:
+            url = newspaper_base_url + "city-news/" + str(index)
+        elif j == 2:
+            url = newspaper_base_url + "country/" + str(index)
+        elif j == 3:
+            url = newspaper_base_url + "international-news/" + str(index)
+        elif j == 4:
+            url = newspaper_base_url + "entertainment/" + str(index)
+        elif j == 5:
+            url = newspaper_base_url + "sports/" + str(index)
+        elif j == 6:
+            url = newspaper_base_url + "mixter/" + str(index)
+        elif j == 7:
+            url = newspaper_base_url + "chayer-desh/" + str(index)
+        elif j == 8:
+            url = newspaper_base_url + "probash-potro/" + str(index)
