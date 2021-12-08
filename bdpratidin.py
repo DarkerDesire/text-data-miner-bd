dcb19690cc37f0e2dcf19dd2dd5c4d4c193e8d66
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2021-12-08 22:15:30 +0000

    More deps

--- bdpratidin.py
+++ bdpratidin.py
@@ -36,3 +36,24 @@ for i in range(100):
             url = newspaper_base_url + "chayer-desh/" + str(index)
         elif j == 8:
             url = newspaper_base_url + "probash-potro/" + str(index)
+        elif j == 9:
+            url = newspaper_base_url + "campus-online/" + str(index)
+        elif j == 10:
+            url = newspaper_base_url + "facebook/" + str(index)
+        elif j == 11:
+            url = newspaper_base_url + "islam/" + str(index)
+        elif j == 12:
+            url = newspaper_base_url + "minister-spake/" + str(index)
+        elif j == 13:
+            url = newspaper_base_url + "corporate-corner/" + str(index)
+        elif j == 14:
+            url = newspaper_base_url + "chittagong-pratidin/" + str(index)
+        elif j == 15:
+            url = newspaper_base_url + "coronavirus/" + str(index)
+        elif j == 16:
+            url = newspaper_base_url + "Coronal-literature/" + str(index)
+        elif j == 18:
+            url = newspaper_base_url + "open-air-theater/" + str(index)
+        elif j == 19:
+            url = newspaper_base_url + "life/" + str(index)
+        elif j == 20:
