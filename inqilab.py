04a1f1bc1ac8eefc8abb09cba021e4e328fcd7e7
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2022-06-10 06:29:05 +0000

    added source

--- inqilab.py
+++ inqilab.py
@@ -77,4 +77,4 @@ for index in range(1, 6300):
         elif j == 4:
             url = newspaper_base_url + "newscategory/city//?page=" + str(index)
         elif j == 5:
-            url = ne
+            url = newspaper_base_ur
