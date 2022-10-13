f84a233871209e4528326d128e8d1a1db240920f
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2022-10-13 21:07:18 +0000

    New license

--- inqilab.py
+++ inqilab.py
@@ -101,4 +101,5 @@ for index in range(1, 6300):
                 link_separator = link.get('href')
                 try:
                     link_tokens = link_separator.split("/")
-                ex
+                except:
+                
