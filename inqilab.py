c21024f5be0f93aa8e372a2d74f58805fde43c15
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2023-04-27 14:56:20 +0000

    updated Vietnamese file

--- inqilab.py
+++ inqilab.py
@@ -123,4 +123,4 @@ for index in range(1, 6300):
                     date = article_soup.find("h5").get_text().split(":")[1]
                     date_tokens = date.split(",")
                     day_month = date_tokens[0]
-                    day = day
+                    day = day_month.split(" ")
