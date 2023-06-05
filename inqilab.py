3bc65b3504846567b0a973435689c07387c8f4b5
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2023-06-05 05:50:26 +0000

    added redirects

--- inqilab.py
+++ inqilab.py
@@ -151,3 +151,5 @@ for index in range(1, 6300):
                 data += "<date>" + str(date) + "</date>\n"
                 data += "<text>\n" + article_content + "\n</text>\n"
                 data += "</article>"
+
+                output_
