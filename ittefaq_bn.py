bdc1b78223b458a3440575e010776d17cd36dbff
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2023-07-23 11:02:26 +0000

    Improve typing

--- ittefaq_bn.py
+++ ittefaq_bn.py
@@ -10,4 +10,5 @@ newspaper_archive_base_url = 'https://www.ittefaq.com.bd/archives/'
 
 # start_date = date(2019,  4, 30)
 # end_date   = date.today()
-start_date = date(2019, 4, 30
+start_date = date(2019, 4, 30)
+end_date = dat
