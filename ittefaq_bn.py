8d58935afffbd3c776f921e50fdb0702c590c5f7
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2023-07-25 18:57:34 +0000

    Fehler behoben

--- ittefaq_bn.py
+++ ittefaq_bn.py
@@ -13,4 +13,5 @@ newspaper_archive_base_url = 'https://www.ittefaq.com.bd/archives/'
 start_date = date(2019, 4, 30)
 end_date = date.today()
 delta = end_date - start_date
-ou
+output_result = []
+data
