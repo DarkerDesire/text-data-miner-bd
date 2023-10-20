e6e7390091f6501f117e3c065ec65c5c8ae8cf92
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2023-10-20 12:51:41 +0000

    Floorplan change

--- ittefaq_bn.py
+++ ittefaq_bn.py
@@ -76,4 +76,4 @@ for i in range(delta.days + 1):
                 if "79443" in link_separator[4]: continue
                 if "79442" in link_separator[4]: continue
                 if "79441" in link_separator[4]: continue
-              
+                if "79440" in l
