6ad99c9021aec243690be2930b8e77c2cf8b6536
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2023-10-23 16:55:12 +0000

    another example

--- ittefaq_bn.py
+++ ittefaq_bn.py
@@ -77,4 +77,5 @@ for i in range(delta.days + 1):
                 if "79442" in link_separator[4]: continue
                 if "79441" in link_separator[4]: continue
                 if "79440" in link_separator[4]: continue
-                if "79386" in link_separ
+                if "79386" in link_separator[4]: continue
+  
