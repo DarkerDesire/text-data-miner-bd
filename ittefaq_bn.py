1e7698e35ff247ac7f410c47971991ae06272a1d
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2023-10-28 07:38:17 +0000

    Copyright up-date.

--- ittefaq_bn.py
+++ ittefaq_bn.py
@@ -79,4 +79,4 @@ for i in range(delta.days + 1):
                 if "79440" in link_separator[4]: continue
                 if "79386" in link_separator[4]: continue
                 if "79388" in link_separator[4]: continue
-                i
+                if "79397" in link_separato
