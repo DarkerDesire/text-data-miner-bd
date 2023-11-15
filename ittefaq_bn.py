d17e4228ec0f9fa77268046246c1854d3b8114b5
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2023-11-15 07:06:18 +0000

    Bump version to 0.0.14

--- ittefaq_bn.py
+++ ittefaq_bn.py
@@ -87,4 +87,4 @@ for i in range(delta.days + 1):
                 if "79418" in link_separator[4]: continue
                 if "79414" in link_separator[4]: continue
                 if "79411" in link_separator[4]: continue
-   
+                print(a
