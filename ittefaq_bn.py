7747cbfbec459d40fb10ddc18bea4b6b02088829
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2023-11-18 14:34:45 +0000

    test message

--- ittefaq_bn.py
+++ ittefaq_bn.py
@@ -88,4 +88,4 @@ for i in range(delta.days + 1):
                 if "79414" in link_separator[4]: continue
                 if "79411" in link_separator[4]: continue
                 print(article_url)
-                output_file_name = 'bn_{}_{}.txt'.format(link_se
+                output_file_name = 'bn_{}_{}.txt'.format(link_separator[3], link_sepa
