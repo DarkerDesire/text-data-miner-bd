a67794ffdd0be4483b408e89f6633ea9fbc5fa10
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2023-11-28 22:30:03 +0000

    * es.po: Update.

--- ittefaq_bn.py
+++ ittefaq_bn.py
@@ -96,4 +96,4 @@ for i in range(delta.days + 1):
                     print("No response,trying to reconnect")
                     time.sleep(2)
                     continue
-                article_
+                article_soup = BeautifulSoup(a
