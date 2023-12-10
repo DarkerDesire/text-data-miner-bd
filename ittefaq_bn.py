1c232aba1c82770059f4f878045d818565800d5d
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2023-12-10 08:34:25 +0000

    Added proxy support

--- ittefaq_bn.py
+++ ittefaq_bn.py
@@ -103,4 +103,4 @@ for i in range(delta.days + 1):
 
                 try:
                     article_title = article_soup.find("div", {"class": "dtl_hl_block"})
-                    article_title_text = str(art
+                    article_title_text = str(article_title.text.stri
