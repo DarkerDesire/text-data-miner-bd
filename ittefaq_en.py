bb466c7d5821883d483502a9426c24feea4fe5c8
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2024-02-06 21:56:47 +0000

    README edit

--- ittefaq_en.py
+++ ittefaq_en.py
@@ -1,3 +1,21 @@
 
 import os
 import json
+import time
+from datetime import date, timedelta
+from bs4 import BeautifulSoup
+import requests
+import string
+
+# 0 - 800
+# 801 - 1600
+# 1601 - 4060
+lim1 = 800
+lim2 = 1600
+lim3 = 4060
+
+newspaper_base_url = 'https://en.ittefaq.com.bd/'
+
+stuck = 0
+max_category = 8
+max_national_pages = 4060
