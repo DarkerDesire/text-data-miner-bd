5b4f4e1a2b656f059f5c1df3acfc9fdbe0de4759
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2024-02-08 09:03:49 +0000

    vol

--- ittefaq_en.py
+++ ittefaq_en.py
@@ -19,3 +19,28 @@ newspaper_base_url = 'https://en.ittefaq.com.bd/'
 stuck = 0
 max_category = 8
 max_national_pages = 4060
+max_politics_pages = 729
+max_international_pages = 2593
+max_business_pages = 527
+max_editorial_oped_pages = 94
+max_sports_pages = 1761
+max_sci_tech_pages = 1262
+max_culture_pages = 1331
+myLinks = ""
+output_result = []
+data = []
+all_url = ""
+prev_j = 0
+for i in range(0, lim1):
+    print(str(i + 1))
+    with open("en_log.txt", 'a', encoding='utf8') as file:
+        file.write(str(i + 1) + '\n')
+    for j in range(max_category):
+        if j == 0: url = newspaper_base_url + "national/page/" + str(i + 1)
+        if j == 1:
+            if max_politics_pages >= i + 1:
+                url = newspaper_base_url + "politics/page/" + str(i + 1)
+            else:
+                url = "none"
+        if j == 2:
+            if max_international_pages >= i + 1:
