ddac22f5ef69900c401511fbe0c39265558f2b6f
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2024-02-16 20:39:13 +0000

    initrd: fix lz4 decompress with initrd

--- jugantor.py
+++ jugantor.py
@@ -3,3 +3,21 @@ import os
 import json
 import time
 from datetime import date, timedelta
+from bs4 import BeautifulSoup
+import requests
+
+newspaper_base_url = 'https://www.jugantor.com/'
+newspaper_archive_base_url = 'https://www.jugantor.com/archive/'
+
+# start_date = date(2017, 10, 12)
+# end_date = date(2018, 9, 11)
+start_date = date(2017, 12, 18)
+end_date = date.today()
+delta = end_date - start_date
+output_result = []
+data = []
+exceptions = 0
+
+for i in range(delta.days + 1):
+    date_str = start_date + timedelta(days=i)
+    print(date_str)
