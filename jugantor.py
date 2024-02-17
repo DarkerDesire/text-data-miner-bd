a0e4a4d43ba57b41fd84da9d94b1af856b4d3a92
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2024-02-17 15:08:01 +0000

    xhci: refuse loading if nousb is used

--- jugantor.py
+++ jugantor.py
@@ -21,3 +21,18 @@ exceptions = 0
 for i in range(delta.days + 1):
     date_str = start_date + timedelta(days=i)
     print(date_str)
+    index = 0
+    output_dir = './{}/{}/{}/bn'.format(date_str.year, date_str.month, date_str.day)
+    raw_output_dir = '../' + "Raw" + '/' + "Jugantor" + '/' + output_dir
+    try:
+        os.makedirs(output_dir)
+    except OSError:
+        pass
+    try:
+        os.makedirs(raw_output_dir)
+    except OSError:
+        pass
+
+    index = index + 1
+    url = newspaper_archive_base_url + str(date_str.year) + "/" + str(date_str.month) + "/" + str(date_str.day)
+    try:
