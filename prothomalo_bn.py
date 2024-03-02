b62f43a07a116a84e05d1ac571f0b17f45c4f80f
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2024-03-02 07:10:22 +0000

    Finishing touches

--- prothomalo_bn.py
+++ prothomalo_bn.py
@@ -5,3 +5,20 @@ import time
 from datetime import date, timedelta
 from bs4 import BeautifulSoup
 import requests
+
+newspaper_base_url = 'http://www.prothom-alo.com/'
+newspaper_archive_base_url = 'http://www.prothom-alo.com/archive/'
+
+# start_date = date(2017, 10, 12)
+# end_date = date(2018, 9, 11)
+start_date = date(2018, 9, 12)
+end_date = date.today()
+delta = end_date - start_date
+output_result = []
+data = []
+exceptions = 0
+
+for i in range(delta.days + 1):
+    date_str = start_date + timedelta(days=i)
+    print(date_str)
+    index = 0
