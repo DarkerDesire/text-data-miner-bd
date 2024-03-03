1d47dc7614a167c45fe1e57fb3f984561d456f37
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2024-03-03 14:13:30 +0000

    Update mariadb-galera-install.sh

--- prothomalo_bn.py
+++ prothomalo_bn.py
@@ -22,3 +22,26 @@ for i in range(delta.days + 1):
     date_str = start_date + timedelta(days=i)
     print(date_str)
     index = 0
+    output_dir = './{}/{}/{}/bn'.format(date_str.year, date_str.month, date_str.day)
+    raw_output_dir = '../' + "Raw" + '/' + "Prothom_Alo.com" + '/' + output_dir
+    try:
+        os.makedirs(output_dir)
+    except OSError:
+        pass
+    try:
+        os.makedirs(raw_output_dir)
+    except OSError:
+        pass
+
+    while (True):
+        index = index + 1
+        url = newspaper_archive_base_url + str(date_str) + '?edition=all&page=' + str(index)
+        try:
+            archive_soup = requests.get(url)
+        except:
+            print("No response for links in archive,trying to reconnect")
+            time.sleep(2)
+            continue
+        soup = BeautifulSoup(archive_soup.content, "html.parser")
+        all_links = soup.find_all("a", attrs={"class": "link_overlay"})
+        page_links_length = len(all_links)
