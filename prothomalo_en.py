989c6c892a26d342e69d3b9f65f261a39a75675f
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2024-03-13 16:25:13 +0000

    Add Beerpay's badge

--- prothomalo_en.py
+++ prothomalo_en.py
@@ -2,3 +2,32 @@
 import os
 import json
 import time
+from datetime import date, timedelta
+from bs4 import BeautifulSoup
+import requests
+
+newspaper_base_url = 'https://en.prothomalo.com/'
+newspaper_archive_base_url = 'https://en.prothomalo.com/archive/'
+
+start_date = date(2019, 8, 9)
+end_date = date.today()
+delta = end_date - start_date
+output_result = []
+data = []
+exceptions = 0
+
+for i in range(delta.days + 1):
+    date_str = start_date + timedelta(days=i)
+    print(date_str)
+    index = 0
+    output_dir = './{}/{}/{}/en'.format(date_str.year, date_str.month, date_str.day)
+    raw_output_dir = '../' + "Raw" + '/' + "Prothom_Alo.com" + '/' + output_dir
+    try:
+        os.makedirs(output_dir)
+    except OSError:
+        pass
+
+    try:
+        os.makedirs(raw_output_dir)
+    except OSError:
+        pass
