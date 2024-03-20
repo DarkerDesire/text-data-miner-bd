7b167a021b315fbf96ca45b2faf7b4236a85bbea
DarkerDesire <134212530+DarkerDesire@users.noreply.github.com>
2024-03-20 08:24:33 +0000

    split source file

--- prothomalo_en.py
+++ prothomalo_en.py
@@ -31,3 +31,23 @@ for i in range(delta.days + 1):
         os.makedirs(raw_output_dir)
     except OSError:
         pass
+
+    url = newspaper_archive_base_url + str(date_str)
+    archive_soup = requests.get(url)
+    soup = BeautifulSoup(archive_soup.content, "html.parser")
+    all_links = soup.find_all("a", attrs={"class": ""})
+    page_links_length = len(all_links)
+
+    if page_links_length == 0:
+        break
+    else:
+        for link in all_links:
+            link_separator = link.get('href').split('/')
+            link_separator_len = len(link_separator)
+            if link_separator_len != 5: continue
+            link = link_separator[1] + "/" + link_separator[2] + "/" + link_separator[3] + link_separator[4]
+            output_file_name = 'en_{}{}.txt'.format(link_separator[2], link_separator[3])
+            article_url = newspaper_base_url + link
+            print(article_url)
+            try:
+                article_data = requests.get(article_url)
